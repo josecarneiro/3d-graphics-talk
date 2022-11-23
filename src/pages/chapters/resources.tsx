12d39ef666e8f691b46d5070de5edc1a5059f268
José Carneiro <contact@josecarnei.ro>
2022-11-23 19:31:58 +0000

    Format resources

--- src/pages/chapters/resources.tsx
+++ src/pages/chapters/resources.tsx
@@ -8,8 +8,14 @@ const Page = () => (
         <Link href='https://docs.pmnd.rs/react-three-fiber/getting-started/introduction'>
           React Three Fiber Document
         </Link>
+      </li>
+      <li>
         <Link href='https://twitter.com/0xca0a'>0xca0a</Link>
+      </li>
+      <li>
         <Link href='https://threejs.org/docs/'>Three.js Docs</Link>
+      </li>
+      <li>
         <Link href='https://codesandbox.org/'>CodeSandbox R3F Boilerplate</Link>
       </li>
     </ol>
